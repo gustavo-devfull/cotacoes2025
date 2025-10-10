130133eb291ae7c619c69f8a1e69f9c568bf6fa0
Gustavo Santos <gutopc@gmail.com>
2025-10-10 10:17:06 -0300

    🎨 Troca de fonte: Inter → Funnel Display em todo o projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'inter': ['Inter', 'sans-serif'],
+        'funnel': ['Funnel Display', 'sans-serif'],
       },
       colors: {
         primary: {
